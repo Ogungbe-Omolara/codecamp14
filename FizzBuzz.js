a0b2a80e3717423dfc5b3a9bf9fbfe205ee7ee17
Ogungbe-Omolara <omolaraogungbe@gmail.com>
2014-08-08 19:20:17 +0100

    JavaScript
    FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,22 @@
+var counter = 1;
+
+for( counter = 1; counter <= 100; counter ++){
+    
+    
+    if ((counter % 3 == 0) && (counter % 5 != 0)){
+        console.log("Fizz");
+        
+    }
+    else if ((counter % 5 == 0) && (counter % 3 != 0)){
+        console.log("Buzz");
+            
+    }
+    else if ((counter % 3 == 0) && (counter % 5 == 0)){
+        console.log("FizzBuzz");
+    }
+    
+    else{
+        console.log(counter);
+        
+    }
+}
